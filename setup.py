d7d752afadb3b3d53f689e9f4cae020983767bdd
ohmyocr <ohmyocr@gmail.com>
2021-04-12 17:33:38 +0800

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="text_renderer",
-    version="0.0.1",
+    version="0.1.0",
     author="oh-my-ocr",
     url="https://github.com/oh-my-ocr/text_renderer",
     description="",
