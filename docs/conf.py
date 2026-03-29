46a31da0bf245b72defcee4479120d50fda7b8fa
Bvicii <yizhanhuang2002@gmail.com>
2026-03-29 11:08:52 -0700

    chore: bump version to 0.2.0
    Signed-off-by: Bvicii <yizhanhuang2002@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = "2020, ohmyocr"
 author = "ohmyocr"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "0.2.0"
 
 
 # -- General configuration ---------------------------------------------------
--- text_renderer/__init__.py
+++ text_renderer/__init__.py
@@ -19,4 +19,4 @@ Example:
     image, text = renderer()
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
